9f56c9df6010b7045e3134720cfeb812370dab57
Jon Fuller <fullerjc@gmail.com>
2010-08-20 13:06:24 -0400

    pull fire options into their own test file

--- src/Tests/Examples/ContainerUsage/AutoNotifyAttributeConvention.cs
+++ src/Tests/Examples/ContainerUsage/AutoNotifyAttributeConvention.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 using NUnit.Framework;
 using StructureMap.AutoNotify;
-using Tests.Util;
 using Container = StructureMap.Container;
 
 namespace Tests.Examples.ContainerUsage
@@ -20,21 +19,8 @@ namespace Tests.Examples.ContainerUsage
             }));
 
             var gem = container.GetInstance<IGem>();
-            var rock = container.GetInstance<Rock>();
-
 
             Assert.That(gem, Is.InstanceOf<INotifyPropertyChanged>());
-            Assert.That(rock, Is.InstanceOf<INotifyPropertyChanged>());
-
-
-            // Make sure "FireOptions.OnlyOnChange" got hooked up correctly for Rock
-            var rockTracker = new EventTracker<PropertyChangedEventHandler>();
-
-            rock.Value = "test";
-            (rock as INotifyPropertyChanged).PropertyChanged += rockTracker;
-            rock.Value = "test";
-
-            Assert.That(rockTracker.WasNotCalled);
         }
 
         // note the autonotify attribute goes on the interface, not the class
@@ -47,13 +33,6 @@ namespace Tests.Examples.ContainerUsage
         public class Gem : IGem
         {
             public string Value { get; set; }
-        }
-
-        [AutoNotify(Fire = FireOptions.OnlyOnChange)]
-        public class Rock
-        {
-            // note for autonotify to work, the property must be virtual
-            public virtual string Value { get; set; }
         }
     }
 }
---
+++ src/Tests/Examples/ContainerUsage/AutoNotifyFireOptions.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+using StructureMap.AutoNotify;
+using Tests.Util;
+using Container = StructureMap.Container;
+
+namespace Tests.Examples.ContainerUsage
+{
+    [TestFixture]
+    public class AutoNotifyFireOptions
+    {
+        [Test]
+        public void UsingFireOptionOnlyOnChange()
+        {
+            var container = new Container(config => config.Scan(scanConfig =>
+            {
+                scanConfig.With(new AutoNotifyAttrConvention());
+                scanConfig.TheCallingAssembly();
+                scanConfig.WithDefaultConventions();
+            }));
+
+            var foo = container.GetInstance<Foo>();
+
+            var tracker = new EventTracker<PropertyChangedEventHandler>();
+
+            foo.Value = "test";
+            (foo as INotifyPropertyChanged).PropertyChanged += tracker;
+            foo.Value = "test";
+
+            Assert.That(tracker.WasNotCalled);
+        }
+
+        [Test]
+        public void UsingFireOptionAlways()
+        {
+            var container = new Container(config => config.Scan(scanConfig =>
+            {
+                scanConfig.With(new AutoNotifyAttrConvention());
+                scanConfig.TheCallingAssembly();
+                scanConfig.WithDefaultConventions();
+            }));
+
+            var bar = container.GetInstance<Bar>();
+
+            var tracker = new EventTracker<PropertyChangedEventHandler>();
+
+            bar.Value = "test";
+            (bar as INotifyPropertyChanged).PropertyChanged += tracker;
+            bar.Value = "test";
+
+            Assert.That(tracker.WasCalled);
+        }
+
+        [AutoNotify(Fire = FireOptions.OnlyOnChange)]
+        public class Foo
+        {
+            // note for autonotify to work, the property must be virtual
+            public virtual string Value { get; set; }
+        }
+
+        [AutoNotify(Fire = FireOptions.Always)]
+        public class Bar
+        {
+            // note for autonotify to work, the property must be virtual
+            public virtual string Value { get; set; }
+        }
+    }
+}
