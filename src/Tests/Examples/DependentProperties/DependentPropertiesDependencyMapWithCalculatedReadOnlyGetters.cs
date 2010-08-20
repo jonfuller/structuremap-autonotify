05eaf4a350a55e1a82ffdb7562471670cd4ed845
Jon Fuller <fullerjc@gmail.com>
2010-08-20 13:08:00 -0400

    pull dependent property test examples into their own files

---
+++ src/Tests/Examples/DependentProperties/DependentPropertiesDependencyMapWithCalculatedReadOnlyGetters.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+using StructureMap.AutoNotify;
+using Tests.Util;
+using Container = StructureMap.Container;
+
+namespace Tests.Examples.DependentProperties
+{
+    [TestFixture]
+    public class DependentPropertiesDependencyMapWithCalculatedReadOnlyGetters
+    {
+        [Test]
+        public void UsingDependentPropertiesUpdatesRelatedWhenUsingDependsOnInDependencyMap()
+        {
+            var container = new Container(config => config.Scan(scanConfig =>
+            {
+                scanConfig.With(new AutoNotifyAttrConvention());
+                scanConfig.TheCallingAssembly();
+                scanConfig.WithDefaultConventions();
+            }));
+
+            var project = container.GetInstance<Project>();
+            var projectTracker = new EventTracker<PropertyChangedEventHandler>();
+
+            (project as INotifyPropertyChanged).PropertyChanged += projectTracker;
+            project.Files = new[]{"src1", "src2"};
+
+            Assert.That(projectTracker.CallCount, Is.EqualTo(2)); // one for Files, one for FileCount
+        }
+
+        [AutoNotify(DependencyMap = typeof(ProjectDependency))]
+        public class Project
+        {
+            public virtual string Name { get; set; }
+            public virtual string[] Files { get; set; }
+            public virtual int FileCount { get { return Files.Length; } }
+        }
+
+        class ProjectDependency : DependencyMap<Project>
+        {
+            public ProjectDependency()
+            {
+                Property(x => x.FileCount).DependsOn(x => x.Files);
+            }
+        }
+    }
+}
---
+++ src/Tests/Examples/DependentProperties/DependentPropertiesUsingDependsOnAttributeWithReadOnlyGetters.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+using StructureMap.AutoNotify;
+using Tests.Util;
+using Container = StructureMap.Container;
+
+namespace Tests.Examples.DependentProperties
+{
+    [TestFixture]
+    public class DependentPropertiesUsingDependsOnAttributeWithReadOnlyGetters
+    {
+        [Test]
+        public void UsingDependentPropertiesUpdatesRelatedWhenUsingDependsOnAttribute()
+        {
+            var container = new Container(config => config.Scan(scanConfig =>
+            {
+                scanConfig.With(new AutoNotifyAttrConvention());
+                scanConfig.TheCallingAssembly();
+                scanConfig.WithDefaultConventions();
+            }));
+
+            var account = container.GetInstance<Account>();
+            var projectTracker = new EventTracker<PropertyChangedEventHandler>();
+
+            (account as INotifyPropertyChanged).PropertyChanged += projectTracker;
+            account.AccountName = "big customer";
+            account.ClientId = "1234";
+
+            Assert.That(projectTracker.CallCount, Is.EqualTo(4));
+        }
+
+        [AutoNotify]
+        public class Account
+        {
+            public virtual string AccountName { get; set; }
+            public virtual string ClientId { get; set; }
+
+            [DependsOn("AccountName", "ClientId")]
+            public virtual string AccountId
+            {
+                get { return ClientId + "/" + AccountName; }
+            }
+        }
+    }
+}
