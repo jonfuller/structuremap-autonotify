940d6770cb983fed555f793fb249cb21f25c4b1c
Jon Fuller <fullerjc@gmail.com>
2010-08-26 08:57:30 -0400

    bump patch #

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("structuremap-autonotify")]
 [assembly: AssemblyDescription("A library for dynamically implementing INotifyPropertyChanged")]
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
 
