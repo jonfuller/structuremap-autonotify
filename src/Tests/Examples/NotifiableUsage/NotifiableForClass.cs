ebc39a4ec8a1d4132e5b2340ff2f3423d8a98277
Jon Fuller <fullerjc@gmail.com>
2010-08-27 12:50:29 -0400

    add a couple more example usages for non container

---
+++ src/Tests/Examples/NotifiableUsage/NotifiableForClass.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Castle.DynamicProxy;
+using NUnit.Framework;
+using StructureMap.AutoNotify;
+
+namespace Tests.Examples.NotifiableUsage
+{
+    [TestFixture]
+    public class NotifiableForClass
+    {
+        [Test]
+        public void CanMakeAnObjectNotifiableWithClass()
+        {
+            var notifiableFoo = Notifiable.MakeForClass(typeof(Foo), FireOptions.Always, new object[0], new ProxyGenerator(), DependencyMap.Empty);
+
+            Assert.That(notifiableFoo is INotifyPropertyChanged);
+        }
+
+        public class Foo
+        {
+            public virtual string Value { get; set; }
+        }
+    }
+}
---
+++ src/Tests/Examples/NotifiableUsage/NotifiableForInterface.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Castle.DynamicProxy;
+using NUnit.Framework;
+using StructureMap.AutoNotify;
+
+namespace Tests.Examples.NotifiableUsage
+{
+    [TestFixture]
+    public class NotifiableForInterface
+    {
+        [Test]
+        public void CanMakeAnObjectNotifiableWithInterface()
+        {
+            var foo = new Foo();
+            var notifiableFoo = Notifiable.MakeForInterface(typeof(IFoo), foo, FireOptions.Always, new ProxyGenerator(), DependencyMap.Empty);
+
+            Assert.That(notifiableFoo is INotifyPropertyChanged);
+        }
+
+        public interface IFoo
+        {
+            string Value { get; set; }
+        }
+
+        internal class Foo : IFoo
+        {
+            public string Value { get; set; }
+        }
+    }
+}
